ff8d2e47e289625fde3fac2242695d4986df67dc
csawe1 <calebnureda@gmail.com>
2023-12-20 22:14:32 +0300

    Fully working add semester unit drag and drop form. Pending: function to save semester units(for specific department) and replicate different canvases for different years

--- TimeTable/Unit/urls.py
+++ TimeTable/Unit/urls.py
@@ -6,4 +6,6 @@ app_name = 'Unit'
 urlpatterns = [
     path('', view_semester_unit, name="SemesterUnit-View"),
     path('addSemesterUnit/',add_semester_unit, name="AddSemesterUnit-View"),
+    path('get_lecturers/<int:department_id>/', get_lecturers, name='GetLecturers-View'),
+    path('saveSemesterUnit', save_semester_units, name='SaveSemesterUnit-View'),
 ]
--- TimeTable/Unit/views.py
+++ TimeTable/Unit/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.core import serializers
+from django.http import JsonResponse
+from django.shortcuts import render
+import json
 from Users.models import NewUser
 from .models import SemesterUnit, Unit
 from .forms import SemesterUnitForm
@@ -15,3 +18,30 @@ def add_semester_unit(request):
     form = SemesterUnitForm()
     units = Unit.objects.all()
     return render(request, 'addSemesterUnit.html', {'form': form, 'units':units,})
+
+@login_required(login_url='Users:Signin-View')
+def get_lecturers(request, department_id):
+    print("Department ID is: ", department_id)
+    if department_id:
+        FIELDS = ['id', 'username', 'first_name', 'last_name']
+        lecturers = serializers.serialize("json", NewUser.objects.filter(Department__id=department_id, group='lecturer'), fields=FIELDS)
+        return JsonResponse(lecturers, safe=False)
+    else:
+        print("There") 
+        return JsonResponse({'error': 'Department ID not provided'}, status=400)
+
+@login_required(login_url='Users:Signin-View')  
+def save_semester_units(request):
+    if request.method == 'POST':
+        try:
+            raw_data = json.loads(request.body.decode('utf-8'))
+            print("Raw Data: ", raw_data)
+            semester_units = raw_data.get('semesterUnits', [])
+            for semester_unit in semester_units:
+                print(semester_unit)
+            
+            # Return a success response
+            return JsonResponse({'success': True})
+        except Exception as e:
+            # Return an error response
+            return JsonResponse({'success': False, 'error': str(e)})
