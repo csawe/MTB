4704ea063b7ac1919313edacb3f5e514002f2e60
csawe1 <calebnureda@gmail.com>
2024-01-26 12:10:43 +0300

    Allowed drag and drop of semester units from pallete to canvas. Left: Assign room, (drag to fit end time or form for end time)

--- TimeTable/Schedule/views.py
+++ TimeTable/Schedule/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from School.models import Year
-from Unit.models import SemesterUnit
+from Unit.models import SemesterUnit, Lecture
+from .models import Schedule
 
 # Create your views here.'
 @login_required(login_url='Users:Signin-View')
@@ -11,10 +12,19 @@ def home(request):
 
 @login_required(login_url='Users:Signin-View')
 def create_schedule(request):
-    department = request.user.Department
-    years = Year.objects.filter(Department=department)
-    semesterUnits = SemesterUnit.objects.filter(Year__in=years)
-    context = {
-        'semester_units':semesterUnits,
-    }
-    return render(request, 'schedule.html', context)
+    print(request.user.group)
+    if request.user.group == 'lecturer':
+        department = request.user.Department
+        years = Year.objects.filter(Department=department)
+        semesterUnits = SemesterUnit.objects.filter(Year__in=years)
+        context = {
+            'semester_units':semesterUnits,
+        }
+        return render(request, 'createschedule.html', context)
+    elif request.user.group == 'student':
+        schedule = Schedule.objects.get(Department=request.user.Departmemt)
+        lecutures = Lecture.objects.filter(Department=schedule.Department)
+        context = {
+            'lectures':lecutures,
+        }
+        return render(request, 'schedule.html', context)
